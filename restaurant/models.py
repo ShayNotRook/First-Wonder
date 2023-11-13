fc4a6b4d10261ee458290009f0ada3a45247cfa0
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2023-11-13 11:19:25 +0330

    Initial steps made.

---
+++ restaurant/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+# Create your models here.
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    slug = models.SlugField(max_length=255)
+    
+    def __str__(self):
+        return self.title
+    
+class UserComments(models.Model):
+    
+    ratings = (
+        ( 1 , 'One Star'),
+        ( 2 , 'Two Stars'),
+        ( 3, 'Three Stars'),
+        ( 4 , 'Four Stars'),
+        ( 5 , 'Five Stars') 
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    first_name = models.CharField(max_length=255)
+    rating = models.SmallIntegerField(default=1, choices=ratings)
+    comment = models.TextField(max_length=8000)
+    created_at = models.DateTimeField(default=timezone.now)
+    helpful = models.BooleanField(default=False)
+    reply_to = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.user_id} {self.created_at}"
