658fa51bfa05de7d056b4972558ff80d59926d88
Shayan <140234243+ShayNotRook@users.noreply.github.com>
2023-11-18 17:34:45 +0330

    Additional models added inside models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -53,3 +53,40 @@ class UserComments(models.Model):
     def __str__(self):
         return f"{self.user} {self.created_at}"
     
+    
+
+class Cart(models.Model):
+    item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, default=1, related_name='cart_items')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    description = models.TextField(max_length=1500)
+    quantity = models.SmallIntegerField(null=True)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    total_price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+    class Meta:
+        unique_together = ('item', 'user')
+    
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    delivery = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='delivery_crew', null=True)
+    status = models.BooleanField(db_index=True, default=0)
+    total = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    date = models.DateField(db_index=True)
+    
+    def __str__(self):
+        return self.user
+    
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField(null=True)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    class Meta:
+        unique_together = ('order', 'item')
+    def __str__(self):
+        return self.item
+    
+    
